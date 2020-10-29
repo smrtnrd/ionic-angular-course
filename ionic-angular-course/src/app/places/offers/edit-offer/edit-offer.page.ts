4d837c860644219a26921740863ed9b2d1f1e72c
Buildman Bitja <bbuildman@gmail.com>
2020-10-28 21:37:03 -0400

    Updated places

--- ionic-angular-course/src/app/places/offers/edit-offer/edit-offer.page.ts
+++ ionic-angular-course/src/app/places/offers/edit-offer/edit-offer.page.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Place } from '../../place.model';
-import { ActivatedRoute } from '@angular/router';
-import { NavController } from '@ionic/angular';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NavController, LoadingController } from '@ionic/angular';
 import { PlacesService } from '../../places.service';
 import { FormGroup, Validators, FormControl } from '@angular/forms';
 import { Subscription } from 'rxjs';
@@ -11,47 +11,73 @@ import { Subscription } from 'rxjs';
   templateUrl: './edit-offer.page.html',
   styleUrls: ['./edit-offer.page.scss'],
 })
-export class EditOfferPage implements OnInit {
+export class EditOfferPage implements OnInit, OnDestroy {
   place: Place;
   form: FormGroup;
   private placesSub: Subscription;
-  
+
   constructor(
     private route: ActivatedRoute,
     private placesService: PlacesService,
+    private router: Router,
+    private laodingCtrl: LoadingController,
     private navCtrl: NavController
-  ) { }
+  ) {}
 
   ngOnInit() {
-    this.route.paramMap.subscribe(paramMap => {
+    this.route.paramMap.subscribe((paramMap) => {
       if (!paramMap.has('placeId')) {
         this.navCtrl.navigateBack('/places/tabs/offers');
         return;
       }
-      this.placesSub  = this.placesService.getPlace(paramMap.get('placeId')).subscribe(place => {
-        // get the data from the specific id
-        this.place = place;
-        console.log('PlaceID: ', paramMap.get('placeId'));
-      });
+      this.placesSub = this.placesService
+        .getPlace(paramMap.get('placeId'))
+        .subscribe((place) => {
+          // get the data from the specific id
+          this.place = place;
+          console.log('PlaceID: ', paramMap.get('placeId'));
+        });
       // Reactive Form
       this.form = new FormGroup({
-          title: new FormControl(this.place.title, {
-            updateOn: 'blur',
-            validators: [Validators.required],
-          }),
-          description: new FormControl(this.place.description, {
-            updateOn: 'blur',
-            validators: [Validators.required, Validators.maxLength(180)],
-          })
+        title: new FormControl(this.place.title, {
+          updateOn: 'blur',
+          validators: [Validators.required],
+        }),
+        description: new FormControl(this.place.description, {
+          updateOn: 'blur',
+          validators: [Validators.required, Validators.maxLength(180)],
+        }),
       });
     });
   }
 
+  ngOnDestroy() {
+    if (this.placesSub) {
+      this.placesSub.unsubscribe();
+    }
+  }
   onUpdateOffer() {
-    if(!this.form.valid){
+    if (!this.form.valid) {
       return;
     }
-    console.log(this.form);
+    this.laodingCtrl
+      .create({
+        message: 'Updating place  ...',
+      })
+      .then((loadingEl) => {
+        loadingEl.present(); // start the waiting animation
+        this.placesService
+          .updatePlace(
+            this.place.id,
+            this.form.value.title,
+            this.form.value.description
+          )
+          .subscribe(() => {
+            console.log(this.form);
+            loadingEl.dismiss(); // close the animation
+            this.form.reset();
+            this.router.navigate(['/places/tabs/offers']);
+          });
+      });
   }
-
 }
--- ionic-angular-course/src/app/places/places.service.ts
+++ ionic-angular-course/src/app/places/places.service.ts
@@ -1,42 +1,42 @@
-import { AuthService } from './../auth/auth.service';
-import { Injectable } from '@angular/core';
-import { Place } from './place.model';
-import { BehaviorSubject } from 'rxjs';
-import { take, map, tap, delay } from 'rxjs/operators';
+import { AuthService } from "./../auth/auth.service";
+import { Injectable } from "@angular/core";
+import { Place } from "./place.model";
+import { BehaviorSubject } from "rxjs";
+import { take, map, tap, delay } from "rxjs/operators";
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class PlacesService {
   private _places = new BehaviorSubject<Place[]>([
     new Place(
-      'p1',
-      'Chateau Laurier',
-      'Le Fairmont Château Laurier est un hôtel cinq étoiles de grande renommée situé au cœur du centre-ville d\'Ottawa',
-      'https://ottawa2016aesatema.files.wordpress.com/2015/09/chateau-laurier-ottawa.jpg',
+      "p1",
+      "Chateau Laurier",
+      "Le Fairmont Château Laurier est un hôtel cinq étoiles de grande renommée situé au cœur du centre-ville d'Ottawa",
+      "https://ottawa2016aesatema.files.wordpress.com/2015/09/chateau-laurier-ottawa.jpg",
       300,
-      new Date('2019-01-01'),
-      new Date('2019-12-31'),
-      'abc'
+      new Date("2019-01-01"),
+      new Date("2019-12-31"),
+      "abc"
     ),
     new Place(
-      'p2',
-      'Ottawa Jail Hostel',
-      'Located in downtown Ottawa, the HI Ottawa Jail Hostel is within easy walking distance of Parliament Hill, the Byward Market and all the major downtown attractions.',
-      'https://res.cloudinary.com/hostelling-internation/image/upload/t_hostel_carousel/f_auto,q_auto/v1557521382/nzzomycdvyg3ixrmmokw.jpg',
+      "p2",
+      "Ottawa Jail Hostel",
+      "Located in downtown Ottawa, the HI Ottawa Jail Hostel is within easy walking distance of Parliament Hill, the Byward Market and all the major downtown attractions.",
+      "https://res.cloudinary.com/hostelling-internation/image/upload/t_hostel_carousel/f_auto,q_auto/v1557521382/nzzomycdvyg3ixrmmokw.jpg",
       78,
-      new Date('2019-01-01'),
-      new Date('2019-12-31'),
-      'abc'
+      new Date("2019-01-01"),
+      new Date("2019-12-31"),
+      "abc"
     ),
     new Place(
-      'p3',
-      'The Westin Ottawa',
-      'No matter your reason for visiting Ontario, Canada, whether business or leisure, The Westin Ottawa is dedicated to making your stay in downtown Ottawa',
-      'https://postmediaottawacitizen2.files.wordpress.com/2017/01/the-westin-ottawa-gave-its-customers-this-picture-as-a-thank.jpeg?quality=100&strip=all&w=564',
+      "p3",
+      "The Westin Ottawa",
+      "No matter your reason for visiting Ontario, Canada, whether business or leisure, The Westin Ottawa is dedicated to making your stay in downtown Ottawa",
+      "https://postmediaottawacitizen2.files.wordpress.com/2017/01/the-westin-ottawa-gave-its-customers-this-picture-as-a-thank.jpeg?quality=100&strip=all&w=564",
       198.99,
-      new Date('2019-01-01'),
-      new Date('2019-12-31'),
-      'abc'
+      new Date("2019-01-01"),
+      new Date("2019-12-31"),
+      "abc"
     ),
   ]);
 
@@ -44,11 +44,58 @@ export class PlacesService {
     return this._places.asObservable();
   }
 
-  constructor(private authService: AuthService ) {}
+  constructor(private authService: AuthService) {}
 
   getPlace(id: string) {
-    return this.places.pipe(take(1), map(places => {
-      return { ...places.find((p) => p.id === id) }; // {...} return a clone of the object requested
+    return this.places.pipe(
+      take(1),
+      map((places) => {
+        return { ...places.find((p) => p.id === id) }; // {...} return a clone of the object requested
+      })
+    );
+  }
+
+  editPlace(id: string, title: string, description: string) {
+    /* return this.places.pipe(take(1), map(places => {
+      const place = places.find((p) => p.id === id);
+      place.title = title;
+      place.description = description;
+      console.log('editPlace', place);
+      return place;
+    })); */
+
+    return this.places.pipe(
+      take(1),
+      delay(1000),
+      tap((places) => {
+        setTimeout(() => {
+          const place = places.find((p) => p.id === id);
+          place.title = title;
+          place.description = description;
+          console.log('editPlace', place);
+          return place;
+        }, 1000);
+      })
+    );
+  }
+
+  updatePlace(placeId: string, title: string, description: string) {
+    return this.places.pipe(take(1), tap(places => {
+      // the idea here is to modify the Place object in our list of places
+      const updatedPlaceIndex = places.findIndex(pl => pl.id === placeId);
+      const updatedPlaces = [... places];
+      const oldPlace = updatedPlaces[updatedPlaceIndex];
+      updatedPlaces[updatedPlaceIndex] = new Place(
+        oldPlace.id,
+        title,
+        description,
+        oldPlace.imageUrl,
+        oldPlace.price,
+        oldPlace.availableFrom,
+        oldPlace.availableTo,
+        oldPlace.userId
+      );
+      this._places.next(updatedPlaces);
     }));
   }
 
@@ -64,23 +111,24 @@ export class PlacesService {
       Math.random().toString(),
       title,
       description,
-      'https://ottawa2016aesatema.files.wordpress.com/2015/09/chateau-laurier-ottawa.jpg',
+      "https://ottawa2016aesatema.files.wordpress.com/2015/09/chateau-laurier-ottawa.jpg",
       price,
       dateFrom,
       dateTo,
       this.authService.userId
-    )
+    );
     return this.places.pipe(
       take(1),
       delay(1000),
-      tap(places => {
+      tap((places) => {
         // we use tap instead of subscribe because we returning the element de
         // take the current array of places
         setTimeout(() => {
           this._places.next(places.concat(newPlace));
-        // emit the new array of places with the place that I added
+          // emit the new array of places with the place that I added
         }, 1000); // add a timer  to mimic server conncection
-      }));
+      })
+    );
     // this.places.push(newPlace);
   }
 }
